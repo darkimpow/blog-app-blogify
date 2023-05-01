7a3f03f28f0a3635a91f60cad51a6b958e49e446
Darkim Powell <114354240+darkimpow@users.noreply.github.com>
2023-05-01 16:39:43 -0400

    models directory updated for api folder

--- api/index.js
+++ api/index.js
@@ -4,13 +4,15 @@ const app = express();
 
 
 app.use(cors());
+app.use(express.json());
 app.post('/register', (req,res) =>{
 const {username,password} =req.body;
-res.json('test ok');
+res.json({requestData:{username,password}});
 
 });
 app.listen(4000);
-
+//mongodb+srv://darkimpowell:j6r7SEwEVEKJSbt1@cluster1.ucvvlej.mongodb.net/?retryWrites=true&w=majority
+//j6r7SEwEVEKJSbt1
 
 
 
