63b26c515dc8aee54b0a9caeb98371f83b2c68a6
Darkim Powell <114354240+darkimpow@users.noreply.github.com>
2023-05-01 19:07:49 -0400

    index page js within api folder updated with user cookie and json web token login info

--- api/index.js
+++ api/index.js
@@ -4,11 +4,18 @@ const mongoose = require("mongoose");
 const app = express();
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
+const {json} = require("express");
 const salt = bcrypt.genSaltSync(10);
+const secret = 'aaondswoie3243';
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
-app.use(cors());
+
+app.use(cors({credentials:true,origin:'http://localhost:3000' }));
 app.use(express.json());
+app.use(cookieParser());
 
+//post for registration
  mongoose.connect('mongodb+srv://darkimpowell:j6r7SEwEVEKJSbt1@cluster1.ucvvlej.mongodb.net/?retryWrites=true&w=majority');
 app.post('/register', async (req,res) =>{
 const {username,password} =req.body;
@@ -19,9 +26,33 @@ try{const userDoc = await User.create({username, password: bcrypt.hashSync(passw
 res.status(400).json(e)
 }
 });
+
+//post for login information
+app.post('/login', async (req,res)=> {
+const{username,password} =req.body;
+const userDoc = await User.findOne({username});
+const passOk= bcrypt.compareSync(password, userDoc.password);
+if (passOk){
+    //logged in
+    jwt.sign({username, id: userDoc._id}, secret, {}, (err,token) => {
+    if (err) throw err;
+
+    res.cookie('token',token).json('ok');
+    });
+}else {
+    res.status(400).json('wrong');
+}
+});
+
+app.get('/profile',(req,res ) =>{
+const {token}= req.cookies;
+jwt.verify(token, secret, {}, (err,info ) => {
+if (err) throw err;
+res.json(info);
+});
+});
 app.listen(4000);
-//mongodb+srv://darkimpowell:j6r7SEwEVEKJSbt1@cluster1.ucvvlej.mongodb.net/?retryWrites=true&w=majority
-//j6r7SEwEVEKJSbt1
+
 
 
 
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Link} from "react-router-dom";
 
 function Header() {
+    useEffect(() => {
+        fetch('http://localhost:400/profile',{
+        credentials: 'include',
+        })
+    }, []);
     return (
         <header>
             <Link to="/" className="logo">Blogify.io</Link>
