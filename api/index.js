ec764093e4e01c4f83b1fd9f2cdd5f359d8d5a03
Darkim Powell <114354240+darkimpow@users.noreply.github.com>
2023-05-01 16:57:50 -0400

    user unique login with mongoose parameters & mongo db database secure import tested

--- api/index.js
+++ api/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
 const app = express();
-
+const User = require('./models/User');
 
 app.use(cors());
 app.use(express.json());
-app.post('/register', (req,res) =>{
-const {username,password} =req.body;
-res.json({requestData:{username,password}});
 
+ mongoose.connect('mongodb+srv://darkimpowell:j6r7SEwEVEKJSbt1@cluster1.ucvvlej.mongodb.net/?retryWrites=true&w=majority');
+app.post('/register', async (req,res) =>{
+const {username,password} =req.body;
+try{const userDoc = await User.create({username, password});
+    res.json(userDoc);
+} catch (e) {
+res.status(400).json(e)
+}
 });
 app.listen(4000);
 //mongodb+srv://darkimpowell:j6r7SEwEVEKJSbt1@cluster1.ucvvlej.mongodb.net/?retryWrites=true&w=majority
