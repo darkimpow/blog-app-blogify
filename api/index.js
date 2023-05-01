38ad021eaf3edea5cda66bba8bd33e0ffeb00359
Darkim Powell <114354240+darkimpow@users.noreply.github.com>
2023-05-01 17:53:02 -0400

    Included yarn bcrypt for user password protection within mongo db database

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const mongoose = require("mongoose");
 const app = express();
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -10,9 +12,10 @@ app.use(express.json());
  mongoose.connect('mongodb+srv://darkimpowell:j6r7SEwEVEKJSbt1@cluster1.ucvvlej.mongodb.net/?retryWrites=true&w=majority');
 app.post('/register', async (req,res) =>{
 const {username,password} =req.body;
-try{const userDoc = await User.create({username, password});
+try{const userDoc = await User.create({username, password: bcrypt.hashSync(password,salt),});
     res.json(userDoc);
 } catch (e) {
+    console.log(e);
 res.status(400).json(e)
 }
 });
