0a7be501ca52d70d4eaea2c002b74e250bb52a48
Darkim Powell <114354240+darkimpow@users.noreply.github.com>
2023-05-03 16:01:09 -0400

    index page js within api folder updated with user cookie and json web token login info

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,18 +1,34 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
+import {response} from "express";
 
 function Header() {
+    const [username,setUsername] = useState(null);
     useEffect(() => {
         fetch('http://localhost:400/profile',{
         credentials: 'include',
-        })
+        }).then(response => {
+             response.json().then(userInfo => {
+            setUsername(userInfo.username);
+             })
+        });
     }, []);
     return (
         <header>
             <Link to="/" className="logo">Blogify.io</Link>
             <nav>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
+                {username && (
+                    <>
+                        <Link to={"/create"}>Create new post</Link>
+                        <a>Logout</a>
+                    </>
+                )}
+                {!username &&(
+                  <>
+                      <Link to="/login">Login</Link>
+                      <Link to="/register">Register</Link>
+                  </>
+                )}
             </nav>
         </header>
     );
