5abd57fa1342cf10d98491bb47c89537fd01dcc2
Darkim Powell <114354240+darkimpow@users.noreply.github.com>
2023-05-09 18:30:29 -0400

    post page js line 27 as follows () => { In this code, the optional chaining operator (?.) is used to access the username property of the author object without causing an error if author is undefined. If author is undefined, the default value 'Unknown' is used instead. Similarly, the optional chaining operator is used to access the _id property of the author object in the conditional rendering of the edit button }

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -23,8 +23,8 @@ export default function PostPage() {
         <div className="post-page">
             <h1>{postInfo.title}</h1>
             <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
-            <div className="author">by @{postInfo.author.username}</div>
-            {userInfo.id === postInfo.author._id && (
+            <div className="author">by @{postInfo.author?.username ?? 'Unknown'}</div>
+            {userInfo.id === postInfo.author?._id && (
                 <div className="edit-row">
                     <Link className="edit-btn" to={`/edit/${postInfo._id}`}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
