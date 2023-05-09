ff5ca968982d6b9fd76ba8f455fa088b08533ca4
Darkim Powell <114354240+darkimpow@users.noreply.github.com>
2023-05-09 18:16:43 -0400

    line 18 in post js corrected with author && author.username added to ensure that the author object exists before accessing its username property. This will prevent the "Cannot read properties of undefined (reading 'username')" error from occurring.

--- client/src/UserContext.js
+++ client/src/UserContext.js
@@ -1,10 +1,10 @@
 import {createContext, useState} from "react";
 
-export const UserContext = createContext ({});
+export const UserContext = createContext({});
 
 export function UserContextProvider({children}) {
-    const [userInfo, setUserInfo] =useState({});
-    return(
+    const [userInfo,setUserInfo] = useState({});
+    return (
         <UserContext.Provider value={{userInfo,setUserInfo}}>
             {children}
         </UserContext.Provider>
